c033c6df898dd2b2a1981ceb92a57590c24f5fc6
Jens Birkbak Aagaard <jbaa14@student.aau.dk>
2019-10-11 15:32:56 +0200

    target click func

--- build/main.js
+++ build/main.js
@@ -5,17 +5,58 @@ var gameOptions = {
         width: 600
     }
 };
-function initializeGame() {
-    UI.gameDiv.style.height = String(gameOptions.gameSize.height + "px");
-    UI.gameDiv.style.width = String(gameOptions.gameSize.width + "px");
-}
-initializeGame();
-function createTarget(type) {
+function createTargetDiv(type) {
     if (type === void 0) { type = "target"; }
     var target = document.createElement("div");
     target.classList.add(type);
     return target;
 }
+var Target = /** @class */ (function () {
+    function Target(props) {
+        this.options = {
+            position: "fixed",
+            x: 0,
+            y: 0
+        };
+        this.size = 50;
+        this.delay = 0;
+        this.lifeTime = 10.0;
+        this.locks = 0;
+        this.options = props.options;
+        this.size = props.size;
+        this.delay = props.delay;
+        this.lifeTime = props.lifeTime;
+        this.locks = props.locks;
+        this.create();
+    }
+    Target.prototype.create = function (context) {
+        var _this = this;
+        if (context === void 0) { context = this; }
+        this.div = document.createElement("div");
+        this.div.classList.add("target");
+        this.div.addEventListener('mousedown', function () { return _this.clicked(); });
+        UI.gameDiv.append(this.div);
+    };
+    Target.prototype.kill = function (div) {
+        if (div === void 0) { div = this.div; }
+        this.div.style.animation = 'explode 70ms forwards';
+        setTimeout(function () {
+            UI.gameDiv.removeChild(div);
+        }, 70);
+    };
+    Target.prototype.clicked = function () {
+        this.kill();
+    };
+    return Target;
+}());
+function initializeGame() {
+    UI.gameDiv.style.height = String(gameOptions.gameSize.height + "px");
+    UI.gameDiv.style.width = String(gameOptions.gameSize.width + "px");
+}
+initializeGame();
+var targetlist = [
+    new Target({})
+];
 function startGame() {
 }
 startGame();
--- src/main.ts
+++ src/main.ts
@@ -8,12 +8,59 @@ let gameOptions = {
 
 };
 
-class Target {
-    size: number;
-    delay: number;
-    lifeTime: number;
-    locks: number;
+function createTargetDiv(type:string = "target") {
+    let target = document.createElement("div");
+    target.classList.add(type);
+    return target;
 }
+
+
+class Target {
+
+    options: any = {
+        position: "fixed",
+        x: 0,
+        y: 0
+    };
+    size: number = 50;
+    delay: number = 0;
+    lifeTime: number = 10.0;
+    locks: number = 0;
+    div;
+
+    constructor(props) {
+        this.options = props.options;
+        this.size = props.size;
+        this.delay = props.delay;
+        this.lifeTime = props.lifeTime;
+        this.locks = props.locks;
+        this.create();
+    }
+
+    private create(context = this){
+        this.div = document.createElement("div");
+        this.div.classList.add("target");
+        this.div.addEventListener('mousedown',() => this.clicked());
+        UI.gameDiv.append(this.div);
+        if(this.options.position === "fixed"){
+            //this.div.style.top = this.options.x;
+        }
+    }
+
+    public kill(div = this.div){
+        this.div.style.animation = 'explode 70ms forwards';
+        setTimeout(function () {
+            UI.gameDiv.removeChild(div);
+        }, 70);
+
+    }
+
+    public clicked(){
+
+        this.kill();
+    }
+}
+
 
 function initializeGame() {
     UI.gameDiv.style.height = String(gameOptions.gameSize.height+"px");
@@ -21,14 +68,18 @@ function initializeGame() {
 }
 initializeGame();
 
-function createTargetDiv(type:string = "target") {
-    let target = document.createElement("div");
-    target.classList.add(type);
-    return target;
-}
 
+let targetlist = [
+    new Target({
+        size:50,
+        delay:0,
+        lifeTIme:3,
+        locks:0
+    })
+];
 function startGame() {
 
 }
 startGame();
+
 
